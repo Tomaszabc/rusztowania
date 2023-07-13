fec0c0b9abfb6efb2ba90f122a1cadd390da3f2e
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2023-07-13 11:28:21 +0200

    before order

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -2,6 +2,17 @@ class CartsController < ApplicationController
 
   def show
     @cart = Cart.find(params[:id])
+    @cart_items = @cart.cart_items.includes(:part)
+    @total_weight= calculate_total_weight
   end
 
+  private
+
+  def calculate_total_weight
+    total_weight = 0
+    @cart_items.each do |cart_item|
+      total_weight += cart_item.part.weight * cart_item.quantity
+    end
+    total_weight
+  end
 end
