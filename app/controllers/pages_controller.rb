6e71427685ab7da0afb1ccd388b111a6e6ab6b58
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2023-07-13 09:40:07 +0200

    you have X in your cart

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,20 @@
 class PagesController < ApplicationController
+  before_action :set_cart
 
   def index
     @parts = Part.all
     @parts = @parts.where(system: params[:system]) if params[:system]
     @parts = @parts.where(category: params[:category]) if params[:category]
     @part = Part.find_by(description: params[:description], category: params[:category], system: params[:system]) if params[:description]
-   
   end
 
+  private
+
+  def set_cart
+    @cart = Cart.find_by(id: session[:cart_id])
+    unless @cart
+      @cart = Cart.create
+      session[:cart_id] = @cart.id
+    end
+  end
 end
