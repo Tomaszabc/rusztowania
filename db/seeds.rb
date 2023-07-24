9c9b012722a9b6a9736990769c412f605e7b9fff
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2023-07-24 16:21:25 +0200

    chrome button mystery

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,44 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Part.create!(name: '112926', description: 'Diagonal for stender UBK 100/200', weight: 2.0, system: 'peri', category: 'diagonal', multipack: 1 )
-Part.create!(name: '115354', description: 'Diagonal for stender UBK 125/100', weight: 3.0, system: 'peri', category: 'diagonal', multipack: 1 )
-Part.create!(name: '112765', description: 'Diagonal for stender UBK 125/200', weight: 3.4, system: 'peri', category: 'diagonal', multipack: 1 )
-Part.create!(name: '100981', description: 'Diagonal for stender UBK 150/100', weight: 3.8, system: 'peri', category: 'diagonal', multipack: 1 )
-Part.create!(name: '100973', description: 'Diagonal for stender UBK 150/150', weight: 3.0, system: 'peri', category: 'diagonal', multipack: 1 )
-Part.create!(name: '100572', description: 'Diagonal for stender UBK 150/200', weight: 4.0, system: 'peri', category: 'diagonal', multipack: 1 )
-Part.create!(name: '100985', description: 'Diagonal for stender UBK 200/100', weight: 5.0, system: 'peri', category: 'diagonal', multipack: 1 )
+Part.create!(name: '100057', description: 'DIAGONAL EASY UBL 150/200', weight: 6.38, system: 'peri', category: 'diagonals', multipack: 1 )
+Part.create!(name: '100061', description: 'DIAGONAL EASY UBL 200/200', weight: 7.15, system: 'peri', category: 'diagonals', multipack: 1 )
+Part.create!(name: '100065', description: 'DIAGONAL EASY UBL 250/200', weight: 8.05, system: 'peri', category: 'diagonals', multipack: 1 )
+Part.create!(name: '100069', description: 'DIAGONAL EASY UBL 300/200', weight: 9.04, system: 'peri', category: 'diagonals', multipack: 1 )
+Part.create!(name: '100572', description: 'DIAG. F/STENDER UBK-2 150/200', weight: 7.59, system: 'peri', category: 'diagonals', multipack: 1 )
+Part.create!(name: '133445', description: 'DIAG. F/STENDER UBK-2 200/200', weight: 6.10, system: 'peri', category: 'diagonals', multipack: 1 )
+Part.create!(name: '133454', description: 'DIAG. F/STENDER UBK-2 250/200', weight: 6.76, system: 'peri', category: 'diagonals', multipack: 1 )
+Part.create!(name: '133463', description: 'DIAG. F/STENDER UBK-2 300/200', weight: 7.44, system: 'peri', category: 'diagonals', multipack: 1 )
+Part.create!(name: '130180', description: 'DIAGONAL EASY EBF 250/200', weight: 6.57, system: 'peri', category: 'diagonals', multipack: 1 )
+Part.create!(name: '130189', description: 'DIAGONAL EASY EBF 300/200', weight: 7.46, system: 'peri', category: 'diagonals', multipack: 1 )
 
-Part.create!(name: '104029', description: 'Stalplank UDI 25x50', weight: 5.0, system: 'peri', category: 'stalplank', multipack: 1 )
-Part.create!(name: '104028', description: 'Stalplank UDI 25x75', weight: 5.0, system: 'peri', category: 'stalplank', multipack: 1 )
-Part.create!(name: '104027', description: 'Stalplank UDI 25x100', weight: 5.0, system: 'peri', category: 'stalplank', multipack: 1 )
 
-Part.create!(name: 'UL500', description: 'UNDERLAG 0,5 m', weight: 3.00, system: 'peri', category: 'stender', multipack: 1 )
+Part.create!(name: '130450', description: 'STÅLPLANK EDS 33X 67', weight: 5.42, system: 'peri', category: 'planks', multipack: 1 )
+Part.create!(name: '130448', description: 'STÅLPLANK EDS 33X100', weight: 7.36, system: 'peri', category: 'planks', multipack: 1 )
+Part.create!(name: '130445', description: 'STÅLPLANK EDS 33X150', weight: 10.30, system: 'peri', category: 'planks', multipack: 1 )
 
-Part.create!(name: '137020', description: 'HORISONTAL FORSTERKT UHV-2 150', weight: 9.41, system: 'peri', category: 'horisontal', multipack: 1 )
-Part.create!(name: '137025', description: 'HORISONTAL FORSTERKT UHV-2 200', weight: 12.70, system: 'peri', category: 'horisontal', multipack: 1 )
-Part.create!(name: '137030', description: 'HORISONTAL FORSTERKT UHV-2 250', weight: 15.20, system: 'peri', category: 'horisontal', multipack: 1 )
-Part.create!(name: '137035', description: 'HORISONTAL FORSTERKT UHV-2 300', weight: 18.50, system: 'peri', category: 'horisontal', multipack: 1 )
+Part.create!(name: 'UL500', description: 'UNDERLAG 0,5 m', weight: 3.00, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '100411', description: 'BUNNSKRUE UJB 38-50/30', weight: 3.39, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '100183', description: 'LØFTESIKRING UJS', weight: 0.0, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '133499', description: 'BASISSTENDER UVB 25', weight: 2.26, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '135187', description: 'BASISSTENDER UVB 50', weight: 3.58, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '132219', description: 'STENDER UVR-2 50', weight: 2.49, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '132224', description: 'STENDER UVR-2 100', weight: 4.34, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '132229', description: 'STENDER UVR-2 150', weight: 6.19, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '132234', description: 'STENDER UVR-2 200', weight: 8.03, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '132239', description: 'STENDER UVR-2 300', weight: 11.70, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '137514', description: 'BASE STANDARS EVOTOP EVS 124', weight: 6.50, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '137509', description: 'STANDARD EVOTOP EVM 200', weight: 8.50, system: 'peri', category: 'stenders', multipack: 1 )
+Part.create!(name: '137517', description: 'TOP STANDARD EVOTOP EVT 96', weight: 3.73, system: 'peri', category: 'stenders', multipack: 1 )
+
+
+
+
+
+Part.create!(name: '131995', description: 'HORISONTAL UH-2 25', weight: 1.40, system: 'peri', category: 'horisontals', multipack: 1 )
+Part.create!(name: '133900', description: 'HORISONTAL UH-2 33', weight: 1.59, system: 'peri', category: 'horisontals', multipack: 1 )
+Part.create!(name: '131998', description: 'HORISONTAL UH-2 50', weight: 2.03, system: 'peri', category: 'horisontals', multipack: 1 )
+Part.create!(name: '133903', description: 'HORISONTAL UH-2 67', weight: 2.47, system: 'peri', category: 'horisontals', multipack: 1 )
 
 
 
