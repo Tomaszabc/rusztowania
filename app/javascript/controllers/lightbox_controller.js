6a15aceec6b7af84d210c413156a7c0994dd7e1d
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2023-08-01 12:05:00 +0200

    javascript folders improvement. Resizing images working

--- app/javascript/controllers/lightbox_controller.js
+++
@@ -1,8 +0,0 @@
-export default class extends Stimulus.Controller {
-  connect() {
-    this.element.addEventListener('click', (event) => {
-      event.preventDefault();
-      lightbox.start(event.currentTarget);
-    });
-  }
-}
