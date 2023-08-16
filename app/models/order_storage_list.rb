55cdb2a23e85ce77efe58b34f13f62a1fda0e4ca
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2023-08-16 20:35:28 +0200

    Fix for form_for storages show

--- app/models/order_storage_list.rb
+++ app/models/order_storage_list.rb
@@ -2,7 +2,9 @@ class OrderStorageList < ApplicationRecord
   belongs_to :order
   belongs_to :part
 
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
+  
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "id", "updated_at", "weight", "quantity", "description"]
   end
