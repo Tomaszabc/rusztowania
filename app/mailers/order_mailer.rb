61bbc38d889b5b41955c0d5f54bb58cae4cdc295
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2023-09-05 17:55:08 +0200

    lagermann mailer working

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -7,6 +7,12 @@ class OrderMailer < ApplicationMailer
     user_email = @order.user.email
     mailer_emails = DeliveryEmail.pluck(:recipient_email)
     mailer_emails << user_email unless mailer_emails.include?(user_email)
+
+    site = Site.find_by(name: @order.building_site)
+    if site&.ledermann.present?
+      mailer_emails << site.ledermann.email
+    end
+
     mail(to: mailer_emails, subject:"Order for: #{@order.building_site}, #{@order.building_site_info}")
   end
 
