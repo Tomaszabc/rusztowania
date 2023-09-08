0123110522b366bcacf9b21a6efc9c58b1706f8d
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2023-09-08 11:14:11 +0200

    fix emailing list (without emails to:string)

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -12,16 +12,15 @@ class OrderMailer < ApplicationMailer
     if site&.ledermann.present?
       mailer_emails << site.ledermann.email
     end
-
     mailer_emails = mailer_emails.uniq
-
     mail(to: mailer_emails, subject:"Order for: #{@order.building_site}, #{@order.building_site_info}")
   end
 
 
   def storage_confirmation(order, current_user)
     @order = order
-    mailer_emails = @order.recipients_emails(current_user)
+    mailer_emails_with_desc = @order.recipients_emails(current_user)
+    mailer_emails = mailer_emails_with_desc.map(&:last).uniq
     mail(to: mailer_emails, subject:"Order for: #{@order.building_site}, #{@order.building_site_info}")  end
   
 
