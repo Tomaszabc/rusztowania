967c342856a968917eec7f826051a6756ecdf74e
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-12-11 12:37:05 +0100

    notification instead of flash alert

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,8 +6,10 @@ import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
 import Dialog from '@stimulus-components/dialog'
+import Notification from '@stimulus-components/notification'
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
 application.register('dialog', Dialog)
+application.register('notification', Notification)
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,5 @@ pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/d
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@stimulus-components/dialog", to: "https://ga.jspm.io/npm:@stimulus-components/dialog@1.0.1/dist/stimulus-dialog.mjs"
+pin "@stimulus-components/notification", to: "https://ga.jspm.io/npm:@stimulus-components/notification@3.0.0/dist/stimulus-notification.mjs"
+pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.52.2/dist/index.js"
