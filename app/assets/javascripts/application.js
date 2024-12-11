a26389e0a6a8d25b70ed5d88152d18eccfd67ce0
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-12-11 15:41:05 +0100

    working after back

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,30 @@
 import "@hotwired/turbo-rails";
 import "controllers"
 console.log("Turbo and Stimulus loaded successfully");
+
+
+document.addEventListener("turbo:render", () => {
+  console.log("Turbo render triggered");
+});
+
+document.addEventListener("turbo:load", () => {
+  if (window.lightbox) {
+    lightbox.init();
+  }
+});
+
+document.addEventListener("turbo:load", () => {
+if (typeof lightbox !== "undefined") {
+lightbox.init(); // Initialize Lightbox when the page loads
+}
+});
+
+document.addEventListener("turbo:render", () => {
+if (typeof lightbox !== "undefined") {
+lightbox.init(); // Reinitialize Lightbox when the page is restored from cache
+}
+});
+
+document.addEventListener("turbo:render", () => {
+console.log("Turbo render triggered");
+});
