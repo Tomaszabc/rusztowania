9552064fbd78aed8144cdcdc7ed434bb6f48880b
Tomaszabc <tomasz.borowiec1@pollub.edu.pl>
2024-12-11 11:26:10 +0100

    dialog delete for desktop parts

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,7 +5,9 @@ import { application } from "controllers/application"
 // Eager load all controllers defined in the import map under controllers/**/*_controller
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
+import Dialog from '@stimulus-components/dialog'
 
 // Lazy load controllers as they appear in the DOM (remember not to preload controllers in import map!)
 // import { lazyLoadControllersFrom } from "@hotwired/stimulus-loading"
 // lazyLoadControllersFrom("controllers", application)
+application.register('dialog', Dialog)
